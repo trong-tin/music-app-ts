5d56e5a7ee7ed53176e893afc47f102e5ba40835
trong-tin <trongtinvlyk@gmail.com>
2024-06-09 13:37:00 +0700

    Tạo routes theo mô hình MVC

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Topic from "./models/topicModel";
+import clientRoutes from "./routes/client/indexRoute";
 
 dotenv.config();
 database.connect();
@@ -11,14 +11,9 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-  console.log(topics);
+//Client Routes
+clientRoutes(app);
 
-  res.render("client/pages/topics/index");
-});
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
