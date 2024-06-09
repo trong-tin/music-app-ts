52a281e4d4ccee7beddd2ac8ca7bea75faed8329
trong-tin <trongtinvlyk@gmail.com>
2024-06-09 13:13:15 +0700

    Kết nối database

---
+++ config/system.ts
@@ -0,0 +1,6 @@
+const PATH_ADMIN = "/admin";
+const PATH_CLIENT = "/";
+module.exports = {
+  prefixAdmin: PATH_ADMIN,
+  prefixClient: PATH_CLIENT,
+};
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+database.connect();
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
