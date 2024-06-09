6f9d74edfbbd7f5618a2683e39ce56ff6a07fd54
trong-tin <trongtinvlyk@gmail.com>
2024-06-09 13:00:44 +0700

    Cài đặt pug

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Chủ đề bài hát");
+  res.render("client/pages/topics/index");
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
